24b258afd4177cc2950c4305de6f9a7d334337a0
Julian C <cripinojulian@hotmail.com>
2024-01-17 22:48:19 -0300

    Modificaciones en componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArduinoService } from './arduino.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'telemedicionesfnt';
-}
+export class AppComponent implements OnInit {
+  arduinoData: any;
+
+  constructor(private arduinoService: ArduinoService) {}
+
+  ngOnInit() {
+    console.log("Cargando pagina...")
+    this.getArduinoData();
+    console.log("Pagina cargada")
+  }
+
+  getArduinoData() {
+    this.arduinoService.getDataFromArduino().subscribe(
+      data => {
+        this.arduinoData = data;
+        console.log('Datos recibidos desde Arduino:', this.arduinoData);
+      },
+      error => console.error('Error al obtener datos desde Arduino:', error)
+    );
+  }
